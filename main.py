8e6668672baabd9871906c55017b9dea997be927
Tony <tlepri@rogers.com>
2026-03-10 10:02:19 -0400

    Integrate Tavily search tool

--- main.py
+++ main.py
@@ -1,43 +1,22 @@
-from dotenv import load_dotenv  
-from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
-from langchain_ollama import ChatOllama 
+from dotenv import load_dotenv
+
 load_dotenv()
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools)
 
 
 def main():
     print("Hello from langchain-course!")
-    information = """
-    Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman and entrepreneur known for his 
-    leadership of Tesla, SpaceX, X, and xAI. Musk has been the wealthiest person in the world since 2025; 
-    as of February 2026, Forbes estimates his net worth to be around US$852 billion.
-
-Born into a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; 
-he has Canadian citizenship since his mother was born there. He received bachelor's degrees in 1997 from the 
-University of Pennsylvania before moving to California to pursue business ventures. In 1995, 
-Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, 
-an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. 
-Musk also became an American citizen in 2002.
-
-    """
-
-    summary_template = f"""
-    given the information {information} about a person I want you to create:
-    1. a short summary
-    2. two interesting facts about them
-    """
- 
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
-    )
-
-    llm = ChatOpenAI(temperature=0, model="gpt-5")
-    #llm = ChatOllama(model="llama3.2:latest")
-    chain = summary_prompt_template | llm
-
-    response = chain.invoke(input={"information" : information})
-    print(response.content)
-
+    result = agent.invoke({"messages": HumanMessage(content="search for 3 job postings for an ai engineer using langchain in toronto on linkedin and list thir details  ")})
+    print(result)
 
 
 if __name__ == "__main__":
